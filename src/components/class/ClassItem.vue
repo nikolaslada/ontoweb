217e63454bc6598ac10aa6aa1e1a3eff174489f1
Nikolas Lada <lada.nikolas@feg.eu>
2020-04-30 21:17:38 +0200

    Part of ClassItem component

---
+++ src/components/class/ClassItem.vue
@@ -0,0 +1,31 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'ClassItem',
+  components: {
+  },
+  data() {
+    return {
+      data: {
+        name: 'owl:Thing',
+        created: '2020-01-13 12:00:00',
+        updated: null,
+        translationList: [
+          {
+            iso: 'en',
+            name: 'owl:Thing',
+            description: '',
+          },
+        ],
+        externalUrl: null,
+        definitionList: [
+
+        ],
+        condition: {},
+      },
+    };
+  },
+};
+</script>
