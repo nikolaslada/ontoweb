1df26cca014dd3247cdf2c11f472db373b9777be
Nikolas Lada <lada.nikolas@feg.eu>
2020-09-19 18:29:37 +0200

    Added service api.js for creating Axios instance

---
+++ src/services/api.js
@@ -0,0 +1,73 @@
+import Vue from 'vue';
+import axios from 'axios';
+import qs from 'qs';
+
+import {
+  REQUEST_START,
+  REQUEST_END,
+  GET_LANGUAGE_ISO,
+} from '../stores/MainStore/constants';
+
+import {
+  USE_PROMISE_REJECT,
+} from '../conf/constants';
+
+import {
+  URL,
+  TIMEOUT,
+} from '../conf/api';
+
+const vueInstance = new Vue();
+
+const api = axios.create({
+  baseURL: URL.API_ONTOWEB_BO,
+  timeout: TIMEOUT.API_ONTOWEB_BO,
+  paramsSerializer: (params) => (
+    qs.stringify(
+      params,
+      {
+        arrayFormat: 'repeat',
+        skipNulls: true,
+      },
+    )
+  ),
+});
+
+api.interceptors.request.use((config) => {
+  vueInstance.$noCycleStore.commit(`MainStore/${REQUEST_START}`);
+
+  const languageIso = vueInstance.$noCycleStore.getters[`MainStore/${GET_LANGUAGE_ISO}`];
+  const adjustedConfig = {
+    ...config,
+    headers: {
+      ...(config.headers || {}),
+      common: {
+        'language-iso': languageIso,
+        'Content-Type': 'application/json',
+      },
+    },
+  };
+
+  // content-type header for GET and DELETE method workaround https://github.com/axios/axios/issues/86
+  if (['get', 'delete'].includes(config.method)) {
+    adjustedConfig.data = null;
+  }
+
+  return adjustedConfig;
+});
+
+api.interceptors.response.use((response) => {
+  vueInstance.$noCycleStore.commit(`MainStore/${REQUEST_END}`);
+
+  return response;
+}, (error) => {
+  vueInstance.$noCycleStore.commit(`MainStore/${REQUEST_END}`);
+
+  if (error?.config?.[USE_PROMISE_REJECT]) {
+    return Promise.reject(error);
+  }
+
+  return error;
+});
+
+export default api;
