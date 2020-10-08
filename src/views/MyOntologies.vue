f7eb259f7df1510724e2817abe2fb0834317cc6b
Nikolas Lada <lada.nikolas@feg.eu>
2020-10-08 13:54:39 +0200

    MyOntologyList component displayed in views/MyOntologies.vue

--- src/views/MyOntologies.vue
+++ src/views/MyOntologies.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="container">
+    <MyOntologyList/>
   </div>
 </template>
 
 <script>
+import MyOntologyList from '../components/ontologies/MyOntologyList.vue';
+
 export default {
   name: 'MyOntologies',
+  components: {
+    MyOntologyList,
+  },
 };
 </script>
