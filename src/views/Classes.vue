579b1a2a66c18167558c1cc0f0d4d5c79618e0d1
Nikolas Lada <lada.nikolas@feg.eu>
2020-10-08 12:31:03 +0200

    Added view/Classes.vue

---
+++ src/views/Classes.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <ClassTreeList/>
+    <ClassItem/>
+  </div>
+</template>
+
+<script>
+import ClassTreeList from '../components/classTree/ClassTreeList.vue';
+import ClassItem from '../components/class/ClassItem.vue';
+
+export default {
+  name: 'Classes',
+  components: {
+    ClassTreeList,
+    ClassItem,
+  },
+};
+</script>
