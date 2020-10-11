3112cb687e779c98df7927f07c7eb1a96df8d03c
Nikolas Lada <lada.nikolas@feg.eu>
2020-10-11 17:10:27 +0200

    Removed unused href attributes from NavBar template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,19 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-    <a class="navbar-brand" href="#">{{ BRAND_NAVBAR }}</a>
+    <a class="navbar-brand">{{ BRAND_NAVBAR }}</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <router-link to="/" tag="li" class="nav-item" active-class="active" exact>
-          <a class="nav-link" href="#">{{ NAMES.HOME }}</a>
+          <a class="nav-link">{{ NAMES.HOME }}</a>
         </router-link>
         <router-link to="/my-ontologies" tag="li" class="nav-item" active-class="active">
-          <a class="nav-link" href="#">{{ NAMES.MY_ONTOLOGIES }}</a>
+          <a class="nav-link">{{ NAMES.MY_ONTOLOGIES }}</a>
         </router-link>
         <router-link to="/classes" tag="li" class="nav-item" active-class="active">
-          <a class="nav-link" href="#">{{ NAMES.CLASSES }}</a>
+          <a class="nav-link">{{ NAMES.CLASSES }}</a>
         </router-link>
         <router-link to="/about" tag="li" class="nav-item" active-class="active">
-          <a class="nav-link" href="#">{{ NAMES.ABOUT }}</a>
+          <a class="nav-link">{{ NAMES.ABOUT }}</a>
         </router-link>
         <li v-if="selected.name" class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
